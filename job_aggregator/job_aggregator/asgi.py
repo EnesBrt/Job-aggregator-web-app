6803b3de94c818188245f112e9d925c2f336ae24
EnesBrt <enes.barut@gmx.com>
2024-03-14 20:39:15 +0100

    Update DJANGO_SETTINGS_MODULE in asgi.py

--- job_aggregator/job_aggregator/asgi.py
+++ job_aggregator/job_aggregator/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "job_aggregator.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "job_aggregator.job_aggregator.settings"
+)
 
 application = get_asgi_application()
