aaf757c2dd2dd831cb0e1242a8abb109b22eff25
EnesBrt <enes.barut@gmx.com>
2024-03-14 19:09:56 +0100

    Update database configuration in settings.py

--- job_aggregator/job_aggregator/settings.py
+++ job_aggregator/job_aggregator/settings.py
@@ -82,14 +82,7 @@ from decouple import config
 SECRET_KEY = config("SECRET_KEY")
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
-    }
+    "default": {"default": dj_database_url.config(default=config("DATABASE_URL"))}
 }
 
 
