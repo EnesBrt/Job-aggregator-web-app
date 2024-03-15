0f710eb6cf6ed828841da43275b5c4f77b25a97f
EnesBrt <enes.barut@gmx.com>
2024-03-15 18:00:50 +0100

    Update build and start commands in build.sh and render.yaml

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install dependencies
 pip install -r requirements.txt
 
 # Convert static asset files
 python ./job_aggregator/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
+python ./job_aggregator/manage.py migrate
